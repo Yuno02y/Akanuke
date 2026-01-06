1fa0016e2292956b2fdfacc210bb8d42e86ea992
Yuno02y <126051685+Yuno02y@users.noreply.github.com>
2026-01-06 18:01:14 +0900

    Add files via upload

---
+++ AkanukeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AkanukeApp.swift
+//  Akanuke
+//
+//  Created by K on 2026/01/06.
+//
+
+import SwiftUI
+
+@main
+struct AkanukeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
